9ceab0d766dd33c5b24804dad55df3a2e742fb3b
Konf1ic <114355460+Konf1ic@users.noreply.github.com>
2023-10-31 16:29:19 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,17 +1,22 @@
-create database ecommerces;
+create database accounts;
 
-use ecommerces;
+use accounts;
 
-create table ecommerce(
-id int primary key not null,
-username varchar(255) not null,
-password int not null,
+create table accounts(
+id int auto_increment primary key,
+username varchar(255) not null unique,
+password varchar(255) not null,
 name varchar(255) not null,
-age int not null,
-phone_number varchar(11) not null,
+age int check (age > 18 and age < 60),
+phoneNumber varchar(20) not null,
 address varchar(255) not null,
-wage int not null
+salary decimal(10, 2) check (salary > 0 AND salary < 100000000)
  );
+ 
+ -- Them tk
+INSERT INTO accounts (username, password, email, name, age, phoneNumber, address, salary) 
+VALUES ('newUsername', '123456@Abc', 'newEmail', 'newName', 30, 'newPhoneNumber', 'newAddress', 50000.00);
 
-ALTER TABLE ecommerce ADD CONSTRAINT Tuoi CHECK (age>18 && age<60);
-ALTER TABLE ecommerce ADD CONSTRAINT Luong CHECK (wage>0 && wage<100000000);
+-- Tim theo name hoac user
+SELECT * FROM accounts WHERE username LIKE '%searchTerm%' OR name LIKE '%searchTerm%';
+
